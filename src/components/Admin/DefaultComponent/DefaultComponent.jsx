c39f64a1c99556da5b3d79c1a2768f4bfbf3c11c
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2024-11-30 15:26:31 +0700

    fix

--- src/components/Admin/DefaultComponent/DefaultComponent.jsx
+++ src/components/Admin/DefaultComponent/DefaultComponent.jsx
@@ -5,10 +5,13 @@ import SidebarMenu from '../../Admin/Menu/Sidebar';
 const DefaultComponent = ({ children, title }) => {
   return (
     <div className="flex h-screen">
-      <div className="sticky top-0 h-screen w-32 text-white">
+      {/* Sidebar cố định */}
+      <div className="sticky top-0 h-screen w-32  text-white">
         <SidebarMenu />
       </div>
-      <div className="flex-1 bg-gray-100 ">
+
+      {/* Nội dung chính */}
+      <div className="flex-1 bg-gray-100 overflow-y-auto scrollbar scrollbar-track-gray-100 scrollbar-gray-100">
         <HeaderAdmin pageTitle={title} />
         <div className="container mx-auto p-4">{children}</div>
       </div>
